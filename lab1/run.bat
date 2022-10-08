68655564f000b2966b0edb50035ea4fd09dcbeae
mamsdeveloper <belk1naalyona@yandex.ru>
2022-10-09 02:29:55 +0300

    fix run.sh

--- lab1/run.bat
+++ lab1/run.bat
@@ -1,4 +1,4 @@
 javac -d bin -cp "./libs/Pokemon.jar" -sourcepath src src/s367945/lab1/SimpleTest.java
-jar cfm ./bin/SimpleTest.jar ./manifest.mf *
-# java -cp "./libs/Pokemon.jar;bin" s367945/lab1/SimpleTest
+jar cfm ./SimpleTest.jar ./manifest.mf -C bin/ *
+REM java -cp "./libs/Pokemon.jar;bin" s367945/lab1/SimpleTest
 java -jar SimpleTest.jar
--- lab1/run.sh
+++ lab1/run.sh
@@ -1,3 +1,3 @@
 javac -d bin -cp ./libs/Pokemon.jar -sourcepath src src/s367945/lab1/SimpleTest.java
-jar cfm ./bin/SimpleTest.jar ./manifest.mf *
+jar cfm ./SimpleTest.jar ./manifest.mf -C bin/ *
 java -jar SimpleTest.jar
