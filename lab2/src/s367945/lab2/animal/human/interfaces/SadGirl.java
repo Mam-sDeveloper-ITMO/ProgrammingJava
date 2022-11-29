8c7075d60fb0c1e66308388621638525051c5d7e
mamsdeveloper <belk1naalyona@yandex.ru>
2022-11-29 15:21:48 +0300

    lab3 uml first version

--- lab2/src/s367945/lab2/animal/human/interfaces/SadGirl.java
+++ lab2/src/s367945/lab2/animal/human/interfaces/SadGirl.java
@@ -1,5 +1,11 @@
 package s367945.lab2.animal.human.interfaces;
 
 public interface SadGirl {
+    static class NoReasonsToDie extends RuntimeException {
+        public NoReasonsToDie() {
+            super();
+        }
+    };
+
     void cry();
 }
