d32db5d2d4e9d89545452822486ce3eeb00a44d1
mamsdeveloper <belk1naalyona@yandex.ru>
2022-11-29 11:04:39 +0300

    start lab3

--- lab1/run.bat
+++ lab1/run.bat
@@ -1,4 +1,3 @@
-javac -d bin -cp "./libs/Pokemon.jar" -sourcepath src src/s367945/lab1/SimpleTest.java
-jar cfm ./SimpleTest.jar ./manifest.mf -C bin/ *
-REM java -cp "./libs/Pokemon.jar;bin" s367945/lab1/SimpleTest
+javac -d bin -cp ./libs/Pokemon.jar -sourcepath src src/s367945/lab1/SimpleTest.java
+jar cfm ./SimpleTest.jar ./manifest.mf -C bin/ .
 java -jar SimpleTest.jar
--- lab1/run.sh
+++ lab1/run.sh
@@ -1,3 +1,3 @@
 javac -d bin -cp ./libs/Pokemon.jar -sourcepath src src/s367945/lab1/SimpleTest.java
-jar cfm ./SimpleTest.jar ./manifest.mf -C bin/ *
+jar cfm ./SimpleTest.jar ./manifest.mf -C bin/s367945 .
 java -jar SimpleTest.jar
