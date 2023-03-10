846f89569b239b2d29344eae19013b8aa14a27f2
mamsdeveloper <belk1naalyona@yandex.ru>
2023-03-10 18:25:41 +0300

    add test modules | test build to jar

---
+++ CollectionManager/cliapp/src/main/java/cliapp/App.java
@@ -0,0 +1,10 @@
+package cliapp;
+
+import models.Human;
+
+public class App {
+    public static void main(String[] args) {
+        Human testHuman = new Human();
+        testHuman.greet();
+    }
+}
---
+++ CollectionManager/shared/models/src/main/java/models/Human.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Human {
+    public void greet() {
+        System.out.println("Hello, world");
+    }
+}
