89a49a609cd93fd091a541ab524ec43933ccc346
mamsdeveloper <belk1naalyona@yandex.ru>
2023-03-13 23:50:27 +0300

    shared.humandeque: CsvStorage

--- CollectionManager/shared/humandeque/src/main/java/humandeque/manager/local/CsvStorage.java
+++ CollectionManager/shared/humandeque/src/main/java/humandeque/manager/local/CsvStorage.java
@@ -1,12 +1,22 @@
 package humandeque.manager.local;
 
-import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
 
 import humandeque.HumanDeque;
 import humandeque.manager.CollectionStorage;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import models.Car;
+import models.Coordinates;
+import models.Human;
+import models.Mood;
 
 /**
  * Provide storage of HumanDeque in csv file
@@ -17,18 +27,88 @@ public class CsvStorage implements CollectionStorage {
     private String filePath;
 
     /**
+     * Convert human to strings array
+     */
+    private String[] humanToStrings(Human human) {
+        return new String[] {
+                String.valueOf(human.getId()),
+                human.getName(),
+                String.valueOf(human.getCoordinates().getX()),
+                String.valueOf(human.getCoordinates().getY()),
+                String.valueOf(human.getCreationDate()),
+                String.valueOf(human.isRealHero()),
+                String.valueOf(human.isHasToothpick()),
+                String.valueOf(human.getImpactSpeed()),
+                human.getSoundtrackName(),
+                String.valueOf(human.getMinutesOfWaiting()),
+                String.valueOf(human.getMood()),
+                human.getCar().getName()
+        };
+    }
+
+    /**
+     * Convert strings array to human
+     */
+    private Human stringsToHuman(String[] strings) {
+        return Human.builder()
+                .id(Long.parseLong(strings[0]))
+                .name(strings[1])
+                .coordinates(
+                        Coordinates.builder()
+                                .x(Float.parseFloat(strings[2]))
+                                .y(Float.parseFloat(strings[3]))
+                                .build())
+                .creationDate(LocalDateTime.parse(strings[4]))
+                .realHero(Boolean.parseBoolean(strings[5]))
+                .hasToothpick(Boolean.parseBoolean(strings[6]))
+                .impactSpeed(Double.parseDouble(strings[7]))
+                .soundtrackName(strings[8])
+                .minutesOfWaiting(Float.parseFloat(strings[9]))
+                .mood(Mood.valueOf(strings[10]))
+                .car(new Car(strings[11]))
+                .build();
+    }
+
+    /**
      * Save collection to csv file
      */
     @Override
-    public void save(HumanDeque collection) {
-        Writer writer = new BufferedWriter(new FileWriter(filePath));
+    public void save(HumanDeque collection) throws IOException {
+        CSVWriter writer = new CSVWriter(new FileWriter(filePath));
+
+        String[] header = new String[] {
+                "id", "name", "x", "y", "creationDate", "realHero", "hasToothpick",
+                "impactSpeed", "impactSpeed", "minutesOfWaiting", "mood", "carName"
+        };
+        writer.writeNext(header);
+
+        String[] humanRow;
+        for (Human human : collection) {
+            humanRow = humanToStrings(human);
+            writer.writeNext(humanRow);
+        }
+        writer.close();
     }
 
     /**
      * Load collection from csv file
      */
     @Override
-    public HumanDeque load() {
-        throw new UnsupportedOperationException("Unimplemented method 'load'");
+    public HumanDeque load() throws IOException {
+        CSVReader reader = new CSVReader(new FileReader(filePath));
+        try {
+            String[] nextLine;
+            nextLine = reader.readNext(); // skip header
+
+            Human human;
+            HumanDeque collection = new HumanDeque();
+            while ((nextLine = reader.readNext()) != null) {
+                human = stringsToHuman(nextLine);
+                collection.add(human);
+            }
+            return collection;
+        } catch (CsvException e) {
+            throw new IOException(e);
+        }
     }
 }
---
+++ CollectionManager/shared/humandeque/src/test/java/humandeque/CsvStorageTest.java
@@ -0,0 +1,66 @@
+package humandeque;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import humandeque.manager.local.CsvStorage;
+import models.Car;
+import models.Coordinates;
+import models.Human;
+import models.Mood;
+
+public class CsvStorageTest {
+    File resourcesDirectory = new File("src/test/resources/humandeque");
+
+    @Test
+    public void testSave() {
+        HumanDeque collection = new HumanDeque();
+        Human human = Human.builder()
+                .name("TestName")
+                .coordinates(new Coordinates(1.0f, 2.0f))
+                .realHero(true)
+                .hasToothpick(false)
+                .impactSpeed(1.0)
+                .soundtrackName("Civilian Defense")
+                .minutesOfWaiting(1.0f)
+                .mood(Mood.SADNESS)
+                .car(new Car("TestCar"))
+                .build();
+
+        Human human2 = Human.builder()
+                .name("TestName")
+                .coordinates(new Coordinates(1.0f, 2.0f))
+                .realHero(true)
+                .hasToothpick(false)
+                .impactSpeed(1.0)
+                .soundtrackName("Civilian Defense")
+                .minutesOfWaiting(1.0f)
+                .mood(Mood.SADNESS)
+                .car(new Car("TestCar"))
+                .build();
+        
+        collection.add(human);
+        collection.add(human2);
+
+        CsvStorage storage = new CsvStorage(resourcesDirectory.getPath() + "/testSave.csv");
+        try {
+            storage.save(collection);
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    @Test
+    public void testLoad() {
+        CsvStorage storage = new CsvStorage(resourcesDirectory.getPath() + "/testLoad.csv");
+        try {
+            HumanDeque collection = storage.load();
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+}
