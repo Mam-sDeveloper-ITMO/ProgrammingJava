152256fa1a523d59697bb218be5986ab8b3d2cfe
mamsdeveloper <belk1naalyona@yandex.ru>
2023-03-13 18:40:53 +0300

    update javadoc

--- CollectionManager/shared/humandeque/src/main/java/humandeque/manager/CollectionStorage.java
+++ CollectionManager/shared/humandeque/src/main/java/humandeque/manager/CollectionStorage.java
@@ -2,8 +2,20 @@ package humandeque.manager;
 
 import humandeque.HumanDeque;
 
+/**
+ * 
+ * Interface for collection storages
+ */
 public interface CollectionStorage {
+    /**
+     * Save collection
+     * 
+     * @param collection
+     */
     void save(HumanDeque collection);
 
+    /**
+     * Load collection from storage
+     */
     HumanDeque load();
 }
