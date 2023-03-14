8fab428666fad840c76d9e8eb83d52556e632236
mamsdeveloper <belk1naalyona@yandex.ru>
2023-03-15 01:49:52 +0300

    shared:: init commands module

---
+++ CollectionManager/shared/commands/src/main/java/commands/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Consists abstract command class and stuff for interaction with Command.
+ * 
+ * Can be used for command pattern implementation.
+ * 
+ * Instead of usual command pattern, this implementation provides static and
+ * dynamic access for command`s parameters that can be used for commands with
+ * complex context.
+ */
+
+package commands;
