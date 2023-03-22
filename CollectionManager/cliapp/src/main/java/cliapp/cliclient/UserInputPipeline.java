2a61a84901c13eb6baa94b4b68a59424d812a22f
mamsdeveloper <belk1naalyona@yandex.ru>
2023-03-22 10:38:54 +0300

    cliapp: add inquire to UserInputPipeline

--- CollectionManager/cliapp/src/main/java/cliapp/cliclient/UserInputPipeline.java
+++ CollectionManager/cliapp/src/main/java/cliapp/cliclient/UserInputPipeline.java
@@ -3,6 +3,7 @@ package cliapp.cliclient;
 import java.util.Map;
 import java.util.Scanner;
 
+import cliapp.Messages.CLIClient;
 import commands.requirements.Requirement;
 import commands.requirements.RequirementsPipeline;
 import commands.requirements.exceptions.RequirementAskError;
@@ -20,6 +21,10 @@ public class UserInputPipeline implements RequirementsPipeline {
             if (staticRequirementsMap.containsKey(requirement.getName())) {
                 return requirement.getValue(staticRequirementsMap.get(requirement.getName()));
             }
+            // Ask requirement
+            System.out.print(
+                    CLIClient.ASK_REQUIREMENT.formatted(requirement.getName(), requirement.getDescription()));
+            // Get value
             return requirement.getValue(inputScanner.nextLine());
         } catch (ValidationError e) {
             throw new RequirementAskError(requirement.getName(), e);
