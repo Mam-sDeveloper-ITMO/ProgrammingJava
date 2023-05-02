1e66e61b7267a6b5b8bb66f3c7801bf61b0e19f1
mamsdeveloper <butvin.mihail@yandex.ru>
2023-05-02 12:51:54 +0300

    init collection service

---
+++ CollectionManager/collection-service/src/main/java/service/App.java
@@ -0,0 +1,10 @@
+package service;
+
+/**
+ * The main application class for running the collection service.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Starting collection service...");
+    }
+}
