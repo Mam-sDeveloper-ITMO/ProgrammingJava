63a49557bb080cac23a1b0958362f259070f6e1b
ohtie <butvin.mihail@yandex.ru>
2023-05-11 06:03:14 +0300

    Rename Collections Service | Include in modules

---
+++ CollectionManager/collections-service/src/main/java/collections/App.java
@@ -0,0 +1,37 @@
+package service;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import pingback.Level;
+import pingback.Logger;
+import server.Server;
+import server.dispatcher.Dispatcher;
+import server.routing.Router;
+import service.routers.CollectionsRouter;
+
+/**
+ * The main application class for running the collection service.
+ */
+public class App {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        Logger logger = new Logger(
+                dotenv.get("PINGBACK_APP_URL"),
+                dotenv.get("PINGBACK_API_KEY"),
+                dotenv.get("PINGBACK_PROJECT"),
+                "mainLogger");
+        System.out.println(dotenv.get("PINGBACK_API_KEY"));
+        System.out.println(logger.log("Startup", "Starting up the collection service.", Level.DEBUG));
+
+        Router collectionsRouter = new CollectionsRouter();
+
+        Dispatcher dispatcher = new Dispatcher();
+        dispatcher.includeRouter(collectionsRouter);
+
+        try {
+            Server server = new Server("localhost", 5000, dispatcher);
+            server.run();
+        } catch (Exception e) {
+            logger.log("Server down", e.getMessage(), Level.CRITICAL);
+        }
+    }
+}
---
+++ CollectionManager/collections-service/src/main/java/collections/routers/CollectionsRouter.java
@@ -0,0 +1,18 @@
+package service.routers;
+
+import java.util.Map;
+
+import server.Response;
+import server.routing.Router;
+import server.routing.Trigger;
+
+public class CollectionsRouter extends Router {
+    public CollectionsRouter() {
+        super("collections");
+    }
+
+    @Trigger(trigger = "test")
+    Response test(Map<String, ?> data) {
+        return new Response(true, "test", null);
+    }
+}
